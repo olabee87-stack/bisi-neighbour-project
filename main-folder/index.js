a2c2efaca2817b59d9516af845937174b04920fa
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-05-05 13:26:54 +0300

    added dependencies, createdmongoose connection

--- main-folder/index.js
+++ main-folder/index.js
@@ -1,1 +1,21 @@
-console.log("Bee");
+const express = require("express");
+const app = express();
+const PORT = 8020;
+const mongoose = require("mongoose");
+
+//Database connection specifying exact DB to make a connection to
+mongoose.connect("mongodb://localhost:27017/test");
+
+//Establishing a connection to mongoose
+const db = mongoose.connection;
+db.on("error", (err) => {
+  console.log(`Error occured while connecting to DB:${err}`);
+});
+db.on("open", () => {
+  console.log(`Successfully connected to the DB`);
+});
+
+//Localhost port
+app.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
+});
